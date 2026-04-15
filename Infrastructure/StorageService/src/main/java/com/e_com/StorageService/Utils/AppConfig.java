ad2a961c53906eb5c06414c21e468f7b826699ff
manduong2k2 <manduong2k2@gmail.com>
2026-04-15 22:02:52 +0700

    add gateway registry to storage

--- Infrastructure/StorageService/src/main/java/com/e_com/StorageService/Utils/AppConfig.java
+++ Infrastructure/StorageService/src/main/java/com/e_com/StorageService/Utils/AppConfig.java
@@ -17,7 +17,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class AppConfig {
 
     @Value("${spring.application.auth-domain}")
     private String authDomain;
---
+++ Infrastructure/StorageService/src/main/java/com/e_com/StorageService/Utils/GatewayRegistry.java
@@ -0,0 +1,71 @@
+package com.e_com.StorageService.Utils;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.reactive.function.client.WebClient;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+public class GatewayRegistry {
+    @Autowired
+    @Qualifier("gatewayWebClient")
+    public WebClient gatewayWebClient;
+
+    @Value("${spring.application.name}")
+    private String serviceName;
+
+    @Value("${spring.application.base-url}")
+    private String serviceUrl;
+
+    @PostConstruct
+    public void register() {
+        try {
+            // 1. Check service exists
+            gatewayWebClient.get()
+                    .uri("/services/" + serviceName)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+            gatewayWebClient.put()
+                    .uri("/services/{name}", serviceName)
+                    .bodyValue(Map.of(
+                            "name", serviceName,
+                            "url", serviceUrl))
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+        } catch (Exception e) {
+            // 2. Create service
+            gatewayWebClient.post()
+                    .uri("/services")
+                    .bodyValue(Map.of(
+                            "name", serviceName,
+                            "url", serviceUrl))
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+            // 3. Create routes
+            gatewayWebClient.post()
+                    .uri("/services/" + serviceName + "/routes")
+                    .bodyValue(Map.of(
+                            "paths", List.of("/"),
+                            "strip_path", true))
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+        }
+    }
+}
